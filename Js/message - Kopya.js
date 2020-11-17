ad5477dccefac90941a51b3e725170eb3a84ccae
“deniz-tasdemir” <deniztasdemir34@gmail.com>
2020-11-17 12:20:05 +0200

    javascript dersler tamamı

---
+++ Js/message - Kopya.js
@@ -0,0 +1,12 @@
+function call() {      
+    alert("Beni hangi fonsiyon çağırdı? Tabiki call");
+    
+  }    
+  function Welcome() {      
+    alert("Hoşgeldin");
+    
+  }    
+  function Goodbye() {      
+    alert("Güle Güle");
+    
+  }    
---
+++ Js/message.js
@@ -0,0 +1,24 @@
+function call() {      
+    alert("Beni hangi fonsiyon çağırdı? Tabiki call");
+    
+  }    
+  function Pencere() {      
+    alert("Window");
+    
+  }    
+  function Masa() {      
+    alert("Table");
+    
+  }    
+  function Tahta() {      
+    alert("Board");
+    
+  }  
+  function Silgi() {      
+    alert("Eraser");
+    
+  }  
+  function Kapi() {      
+    alert("door");
+    
+  }  
